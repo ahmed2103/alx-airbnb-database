eaea7119cb6ad4817cd8897fc2b0aa7f52e18000
Ahmed Hasona <123449036+ahmed2103@users.noreply.github.com>
2025-09-08 10:26:25 +0300

    Update aggregations_and_window_functions.sql

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -1,5 +1,33 @@
-select bookings.user_id from bookings group by
-                                          user_id having count(user_id) > 1;
+SELECT
+    u.first_name,
+    u.last_name,
+    COUNT(b.booking_id) AS total_bookings
+FROM
+    "User" AS u
+LEFT JOIN
+    Booking AS b ON u.user_id = b.user_id
+GROUP BY
+    u.user_id, u.first_name, u.last_name
+ORDER BY
+    total_bookings DESC, u.last_name;
 
-select p.property_id, p.name, count(b.booking_id),rank() over (order by count(b.booking_id)) from properties p
-join bookings b on b.property_id = p.property_id group by b.property_id;
+WITH PropertyBookingCounts AS (
+    SELECT
+        p.property_id,
+        p.name AS property_name,
+        COUNT(b.booking_id) AS total_bookings
+    FROM
+        Property AS p
+    LEFT JOIN
+        Booking AS b ON p.property_id = b.property_id
+    GROUP BY
+        p.property_id, p.name
+)
+SELECT
+    property_name,
+    total_bookings,
+    ROW_NUMBER() OVER (ORDER BY total_bookings DESC) AS property_row_number
+FROM
+    PropertyBookingCounts
+ORDER BY
+    property_row_number;
